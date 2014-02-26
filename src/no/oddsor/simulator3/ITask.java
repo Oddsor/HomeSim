508742d809860a4fd7210ee8ef2ce16759401879
Odd Andreas Sørsæther <osorseth@gmail.com>
2014-02-26 04:30:29 +0100

    task-interface begun

---
+++ src/no/oddsor/simulator3/ITask.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package no.oddsor.simulator3;
+
+/**
+ *
+ * @author Odd
+ */
+public interface ITask {
+    public String fulfilledNeed();
+    
+    public boolean available(Time time);
+    
+    public boolean itemsExist(SimulationMap map);
+    
+    public Object[] getRequiredItems();
+}
